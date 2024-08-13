679a1f21ce1eccdbfc86b4e04ca30a7385a03fe6
Me <Me>
2024-08-13 22:11:37 +0200

    ASP.net server added to run whole database as server service

---
+++ Server/Program.cs
@@ -0,0 +1,47 @@
+namespace Server;
+
+using DatabaseNS.Drivers;
+using DatabaseNS.ResultNS;
+
+public class Program
+{
+    private static DatabaseDriver _driver = DatabaseDriver.Instance;
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.MapPost("/query", PostQuery);
+
+        app.Run();
+    }
+
+    public static async Task<IResult> PostQuery(HttpRequest request) {
+        string body = await new StreamReader(request.Body).ReadToEndAsync();
+        Result result = _driver.Execute(body);
+        switch (result.Type) {
+            case ResultType.Ok:
+                return Results.Ok(result.Message);
+            case ResultType.BadRequest:
+                return Results.BadRequest(result.Message);
+            case ResultType.InternalServerError:
+                return Results.StatusCode(500);
+            default:
+                return Results.StatusCode(500);
+        }
+ 
+        
+    }
+
+    /**
+    public static async Task<IResult> GetQuery(HttpRequest request) {
+        string query = await new StreamReader(request.Body).ReadToEndAsync();
+        if (query.Length == 0)
+            return Results.BadRequest("ERROR: Query is missing");
+        else
+            return Results.Ok(query);
+    }
+    **/
+}
